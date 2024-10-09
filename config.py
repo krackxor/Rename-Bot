f3994b47f09adc4247c5eb25d4dc3133db0c499c
krackxor <77487814+krackxor@users.noreply.github.com>
2024-10-09 08:11:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "6510365486"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,13 +14,13 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1001292829118")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002391697478"))
 
 
 # Mongo DB Database Config
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "CodeXBots")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "renamebot")
 
 
 # Other Variables Config
